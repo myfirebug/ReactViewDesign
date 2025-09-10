b18c0013518eaebd52579732c3c46a7f614962e2
hejp <378540660@qq.com>
2025-09-10 11:42:22 +0800

    feat: 完成button组件开发

---
+++ src/components/button/button.tsx
@@ -0,0 +1,46 @@
+import React, { ButtonHTMLAttributes, FC } from "react";
+import classnames from "classnames";
+
+export type ButtonProps = {
+  className?: string;
+  disabled?: boolean;
+  size?: "mini" | "small" | "middle" | "large";
+  color?: "default" | "primary" | "success" | "warning" | "danger";
+  fill?: "solid" | "outline" | "none";
+  block?: boolean;
+  loading?: boolean;
+  shape?: "default" | "rounded" | "rectangular";
+} & ButtonHTMLAttributes<HTMLElement>;
+
+const Button: FC<ButtonProps> = (props) => {
+  const {
+    className,
+    size = "middle",
+    color = "primary",
+    fill = "solid",
+    block,
+    loading,
+    shape = "default",
+    children,
+    disabled,
+    ...rest
+  } = props;
+  const classes = classnames(className, "cms-button", {
+    [`cms-button__${color}`]: color,
+    [`is-${size}`]: size,
+    [`is-fill-${fill}`]: fill,
+    [`is-shape-${shape}`]: shape,
+    "is-block": block,
+    "is-disabled": props.disabled || loading,
+    "is-loading": loading,
+  });
+
+  return (
+    <button className={classes} {...rest} disabled={disabled || loading}>
+      {loading ? <span className="cms-icon">&#xe644;</span> : null}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
