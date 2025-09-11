f2ebb5858e5bee44380613428290c3e98c2508e9
hejp <378540660@qq.com>
2025-09-11 20:22:58 +0800

    fix: 修改按钮文档

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/your-framework";
-
+import { action } from "storybook/actions";
 import Button from "./button";
 
 const meta = {
-  title: "Button 按钮",
+  title: "Button",
   component: Button,
   tags: ["autodocs"],
   argTypes: {
@@ -80,32 +80,42 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Color: Story = {
+  name: "语义按钮",
   render: () => {
     return (
       <>
-        <Button>default</Button>
-        <Button color="primary">primary</Button>
-        <Button color="danger">danger</Button>
-        <Button color="success">success</Button>
-        <Button color="warning">warning</Button>
+        <Button onClick={action("clicked")}>default</Button>
+        <Button color="primary" onClick={action("clicked")}>
+          primary
+        </Button>
+        <Button color="danger" onClick={action("clicked")}>
+          danger
+        </Button>
+        <Button color="success" onClick={action("clicked")}>
+          success
+        </Button>
+        <Button color="warning" onClick={action("clicked")}>
+          warning
+        </Button>
       </>
     );
   },
 };
 export const Size: Story = {
+  name: "按钮尺寸",
   render: () => {
     return (
       <>
-        <Button color="primary" size="mini">
+        <Button color="primary" size="mini" onClick={action("clicked")}>
           mini
         </Button>
-        <Button color="primary" size="small">
+        <Button color="primary" size="small" onClick={action("clicked")}>
           small
         </Button>
-        <Button color="primary" size="middle">
+        <Button color="primary" size="middle" onClick={action("clicked")}>
           middle
         </Button>
-        <Button color="primary" size="large">
+        <Button color="primary" size="large" onClick={action("clicked")}>
           large
         </Button>
       </>
@@ -113,16 +123,17 @@ export const Size: Story = {
   },
 };
 export const Fill: Story = {
+  name: "填充模式",
   render: () => {
     return (
       <>
-        <Button color="primary" fill="solid">
+        <Button color="primary" fill="solid" onClick={action("clicked")}>
           solid
         </Button>
-        <Button color="primary" fill="outline">
+        <Button color="primary" fill="outline" onClick={action("clicked")}>
           outline
         </Button>
-        <Button color="primary" fill="none">
+        <Button color="primary" fill="none" onClick={action("clicked")}>
           none
         </Button>
       </>
@@ -130,16 +141,17 @@ export const Fill: Story = {
   },
 };
 export const Shape: Story = {
+  name: "形状",
   render: () => {
     return (
       <>
-        <Button color="primary" shape="default">
+        <Button color="primary" shape="default" onClick={action("clicked")}>
           default
         </Button>
-        <Button color="primary" shape="rounded">
+        <Button color="primary" shape="rounded" onClick={action("clicked")}>
           rounded
         </Button>
-        <Button color="primary" shape="rectangular">
+        <Button color="primary" shape="rectangular" onClick={action("clicked")}>
           rectangular
         </Button>
       </>
@@ -147,10 +159,11 @@ export const Shape: Story = {
   },
 };
 export const Block: Story = {
+  name: "块级按钮",
   render: () => {
     return (
       <>
-        <Button color="primary" block>
+        <Button color="primary" block onClick={action("clicked")}>
           default
         </Button>
       </>
@@ -158,25 +171,31 @@ export const Block: Story = {
   },
 };
 export const Loading: Story = {
+  name: "加载状态",
   render: () => {
     return (
       <>
-        <Button color="primary" loading>
+        <Button color="primary" loading onClick={action("clicked")}>
           加载中...
         </Button>
-        <Button loading>加载中...</Button>
+        <Button loading onClick={action("clicked")}>
+          加载中...
+        </Button>
       </>
     );
   },
 };
 export const Disabled: Story = {
+  name: "禁用状态",
   render: () => {
     return (
       <>
-        <Button color="primary" disabled>
+        <Button color="primary" disabled onClick={action("clicked")}>
           disabled
         </Button>
-        <Button disabled>disabled</Button>
+        <Button disabled onClick={action("clicked")}>
+          disabled
+        </Button>
       </>
     );
   },
